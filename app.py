2a6dcf16fb4931f7a02f99e448dcbc61dbfd88dd
Aglo <cindysua@gmail.com>
2025-01-21 06:51:38 -0500

    commit4

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import ploty_express as px
+import plotly_express as px
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
@@ -29,3 +29,4 @@ if scatter_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+    
