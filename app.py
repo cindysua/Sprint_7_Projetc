63fe64d72c3e6e1ac9823ad760b32e2d5c771600
Aglo <cindysua@gmail.com>
2025-01-22 05:46:21 -0500

    commit6

--- app.py
+++ app.py
@@ -4,18 +4,13 @@ import plotly_express as px
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
-st.header('Venta de vehículos EEUU')
+st.header('Venta de vehículos EEUU 2018 - 2019')
 
-#hist_button = st.button('Construir histograma') # crear un botón
-
-build_histogram = st.checkbox('Construir un histograma')
-
-if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
+hist_button = st.button('Construir histograma') # crear un botón
         
-#if hist_button: # al hacer clic en el botón
+if hist_button: # al hacer clic en el botón
  # escribir un mensaje
-    #st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -34,4 +29,15 @@ if scatter_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+    # crear un histograma
+    fig2 = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
     
